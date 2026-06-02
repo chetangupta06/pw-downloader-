0dee555b3349d68e3b60a70fafb874303c568f12
Chetan Raj <chetangolma06@gmail.com>
2026-06-02 19:18:19 +0530

    Fix SSE Buffering on Render

--- backend/server.js
+++ backend/server.js
@@ -133,6 +133,7 @@ app.get('/api/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Accel-Buffering', 'no');
   res.flushHeaders();
 
   session.res = res;
